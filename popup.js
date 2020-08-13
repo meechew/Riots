f7977ec46f2dd27689e3160038b8d7d12ece464c
meechew <cbunt@pdx.edu>
2020-08-12 19:41:25 -0700

    Cleaned up code.

--- popup.js
+++ popup.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const FixPageButton = document.getElementById('FixPage');
     FixPageButton.addEventListener('click', function () {
         chrome.tabs.executeScript({
-        //chrome.tabs.getCurrent(function (tab) {
-            code:'let bod = document.body.innerHTML;let fix = bod.replace(/protest/g, "riot");fix = fix.replace(/Protest/g, "Riot");fix = fix.replace(/PROTEST/g, "RIOT");document.body.innerHTML = fix;'
+            //searches the body of a document and replaces
+            //every "protest" with "riot"
+            //TODO: Filter out links to prevent broken links
+            code:"let fix = document.body.innerHTML;" +
+                "let fix = fix.replace(/protest/g, 'riot');" +
+                "fix = fix.replace(/Protest/g, 'Riot');" +
+                "fix = fix.replace(/PROTEST/g, 'RIOT');" +
+                "document.body.innerHTML = fix;"
 
         });
     }, false);
