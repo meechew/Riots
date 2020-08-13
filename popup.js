6f7486af1c454ce7fa5df1bab2cc435a386486a0
meechew <cbunt@pdx.edu>
2020-08-12 20:01:31 -0700

    Cleaned up code.

--- popup.js
+++ popup.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
             //searches the body of a document and replaces
             //every "protest" with "riot"
             //TODO: Filter out links to prevent broken links
-            code:"let fix = document.body.innerHTML;" +
-                "let fix = fix.replace(/protest/g, 'riot');" +
+            code: "let fix = document.body.innerHTML;" +
+                "fix = fix.replace(/protest/g, 'riot');" +
                 "fix = fix.replace(/Protest/g, 'Riot');" +
                 "fix = fix.replace(/PROTEST/g, 'RIOT');" +
                 "document.body.innerHTML = fix;"
