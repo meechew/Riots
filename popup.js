f92fbde7d14772165c7540415008bb13fbf30dd2
meechew <cbunt@pdx.edu>
2020-08-12 21:07:09 -0700

    Cleaned up code.

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const FixPageButton = document.getElementById('FixPage');
     FixPageButton.addEventListener('click', function () {
+        String.prototype.replaceAll = function (s1, s2) {}
         chrome.tabs.executeScript({
             //searches the body of a document and replaces
             //every "protest" with "riot"
