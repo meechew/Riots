0c488459f6ca64f0a52fb154eb9c0cccbb16438e
meechew <cbunt@pdx.edu>
2020-08-12 23:54:24 -0700

    Worked with a REGEX to filter with.

--- popup.js
+++ popup.js
@@ -6,11 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
             //every "protest" with "riot"
             //TODO: Filter out links to prevent broken links
             code: "let fix = document.body.innerHTML;" +
-                "fix = fix.replace(/protest/g, 'riot');" +
-                "fix = fix.replace(/Protest/g, 'Riot');" +
-                "fix = fix.replace(/PROTEST/g, 'RIOT');" +
-                "document.body.innerHTML = fix;"
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(protest)(?![^\"]*\"\\B)/g, 'riot');" +
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(Protest)(?![^\"]*\"\\B)/g, 'Riot');" +
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(PROTEST)(?![^\"]*\"\\B)/g, 'RIOT');" +
+                "document.body.innerHTML = fix;"});
 
-        });
+  /*        This would have filtered by element
+            function ElementalReplace(e) {
+                const list = document.getElementsByTagName(e);
+                let str = "";
+                let k;
+                for (k = 0;k < list.length; ++k) {
+                    list[k].innerHTML = list[k].innerHTML.replace(/protest/g, 'riot');
+                    list[k].innerHTML = list[k].innerHTML.replace(/Protest/g, 'Riot');
+                    list[k].innerHTML = list[k].innerHTML.replace(/PROTEST/g, 'RIOT');
+                }
+            }
+
+        const elements = ['p', 'h1', 'h2', 'h3', 'h4', 'h5']; //,'div'];
+
+        let e;
+        for (e of elements) {
+                ElementalReplace(e);
+            }*/
+
     }, false);
 }, false);
