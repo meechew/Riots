cffdaf24a6ad7798365e562fb75549adf95a7fed
meechew <cbunt@pdx.edu>
2020-08-15 01:26:17 -0700

    refactored for FireFox

---
+++ Riots-Firefox/popup.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const FixPageButton = document.getElementById('FixPage');
+    FixPageButton.addEventListener('click', function () {
+        browser.tabs.executeScript({
+            //searches the body of a document and replaces
+            //every "protest" with "riot" excluding those in quotes
+            code: "let fix = document.body.innerHTML;" +
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(protest)(?![^\"]*\"\\B)/g, 'riot');" +
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(Protest)(?![^\"]*\"\\B)/g, 'Riot');" +
+                "fix = fix.replace(/(?!\\B\"[^\"]*)(PROTEST)(?![^\"]*\"\\B)/g, 'RIOT');" +
+                "document.body.innerHTML = fix;"});
+
+    }, false);
+}, false);
